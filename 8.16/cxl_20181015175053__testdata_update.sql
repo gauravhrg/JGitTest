3ca1187ccaec9e768bb654541fbd6deb6c89e394
gauravhrg <gauravhrg@yahoo.com>
2018-10-16 18:25:30 +0530

    Delete cxl_20181015175053__testdata_update.sql

--- 8.16/cxl_20181015175053__testdata_update.sql
+++
@@ -1,1 +0,0 @@
-UPDATE ADM_ACCESS_LOG SET LOGOUT_DT = to_date('2008-04-21 08:00:11','yyyy-mm-dd hh24:mi:ss') , SESSION_ACTIVE_IND = 2 WHERE SESSION_ACTIVE_IND = 1 AND session_id = 'xrr3fg40otrzck16xrbvgsux' ;
